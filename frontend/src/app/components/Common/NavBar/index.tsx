6e77c9d501c59287e5d7bd9a94b1977260fd4c0c
Jopereira2005 <jopereiravieira98@gmail.com>
2025-10-20 16:58:25 -0300

    add NavBar styles

--- frontend/src/app/components/Common/NavBar/index.tsx
+++ frontend/src/app/components/Common/NavBar/index.tsx
@@ -1,16 +1,43 @@
 "use client";
+import { useState } from "react"
+
 import styled from "./style.module.scss";
 
-interface NavBarProps {
-  maxParkingSpots: number;
-  occupiedSpots: number;
-}
-const NavBar = ({ maxParkingSpots, occupiedSpots }: NavBarProps) => {
+import { HomeIcon } from "@/app/assets/Common/Home";
+import { MarkIcon } from "@/app/assets/Common/Mark";
+import { ProfileIcon } from "@/app/assets/Common/Profile";
+import { SearchIcon } from "@/app/assets/Common/Search";
+
+const NavBar = () => {
+  // const navigate = useNavigate();
+  // const location = useLocation();
+
+  const [page, setPage] = useState(location.pathname);
+
+  const handlePage = (url: string) => {
+    // setPage(url);
+    // navigate(url);
+  }
 
   return (
-    <div className={ styled.nav_bar }>
-      <p className={ styled.nav_bar__text }>Vagas<br/>{ occupiedSpots }/{ maxParkingSpots }</p>
-    </div>
+    <nav className={ styled.navbar }>
+      <div onClick={() => handlePage('/busca')} className={ `${styled.navbar__item} ${page == "/busca" ? styled.navbar__item_active : ''}` }>
+        <SearchIcon className={ styled.icon }/>
+        <h1 className={ styled.text }>Busca</h1>
+      </div>
+      <div onClick={() => handlePage('/inicio')} className={ `${styled.navbar__item} ${page == "/início" ? styled.navbar__item_active : ''}` }>
+        <HomeIcon className={ styled.icon }/>
+        <h1 className={ styled.text }>Início</h1>
+      </div>
+      <div onClick={() => handlePage('/salvos')} className={ `${styled.navbar__item} ${page == "/salvos" ? styled.navbar__item_active : ''}` }>
+        <MarkIcon className={ styled.icon }/>
+        <h1 className={ styled.text }>Salvos</h1>
+      </div>
+      <div onClick={() => handlePage('/perfil')} className={ `${styled.navbar__item} ${page == "/perfil" ? styled.navbar__item_active : ''}` }>
+        <ProfileIcon className={ styled.icon }/>
+        <h1 className={ styled.text }>Perfil</h1>
+      </div>
+    </nav>
   );
 };
 export default NavBar;
